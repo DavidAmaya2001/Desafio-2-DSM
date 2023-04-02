879a58a781d6bf6465a13b53a30ece098b7a5f29
David <gamerbloom5@gmail.com>
2023-04-02 12:19:55 -0600

    Subiendo de nuevo todo

--- desafiopractico2dsm/app/src/main/java/sv/edu/udb/desafiopractico2dsm/HomeActivity.kt
+++ desafiopractico2dsm/app/src/main/java/sv/edu/udb/desafiopractico2dsm/HomeActivity.kt
@@ -2,10 +2,42 @@ package sv.edu.udb.desafiopractico2dsm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
+lateinit var emailShow : TextView
+lateinit var providerShow : TextView
+lateinit var logOut : Button
+
+enum class ProviderType{
+    BASIC
+}
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        emailShow = findViewById(R.id.lblEmail)
+        providerShow = findViewById(R.id.lblProvider)
+        logOut = findViewById(R.id.btnLogOut)
+
+        val bundle = intent.extras
+        val email = bundle?.getString("email")
+        val provider = bundle?.getString("provider")
+
+        configs(email ?: "", provider ?: "")
+    }
+
+    private fun configs(email: String, provider: String){
+        title = "Menu Principal"
+        emailShow.text = email
+        providerShow.text = provider
+
+        logOut.setOnClickListener{
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+
     }
 }
