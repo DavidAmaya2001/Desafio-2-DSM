6e26aae4ca4ab213da5cff0021804fe86a504a8a
David <gamerbloom5@gmail.com>
2023-04-01 22:35:32 -0600

    Inicialización de Proyecto con SignOn y SignOut

---
+++ desafiopractico2dsm/app/src/main/java/sv/edu/udb/desafiopractico2dsm/AuthenticationActivity.kt
@@ -0,0 +1,11 @@
+package sv.edu.udb.desafiopractico2dsm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthenticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
---
+++ desafiopractico2dsm/app/src/main/java/sv/edu/udb/desafiopractico2dsm/MainActivity.kt
@@ -0,0 +1,23 @@
+package sv.edu.udb.desafiopractico2dsm
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, AuthenticationActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+
+    }
+}
